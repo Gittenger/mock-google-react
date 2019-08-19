a8c6ee5c697e131943a16b083e3c2054e6ee826d
Gittenger <john.pittenger86@gmail.com>
2019-08-19 18:42:22 -0400

    search bar and resized logo

--- src/scripts/components/Search.js
+++ src/scripts/components/Search.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Search() {
-  return <div>Search</div>;
+  return (
+    <div className="search-box">
+      <input
+        className="search-bar"
+        placeholder="Search Google or type a URL"
+        type="text"
+      />
+    </div>
+  );
 }
 
 export default Search;
