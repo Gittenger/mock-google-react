c7b116c895afeeb51262909efa7a8cdb8d0ea9aa
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-05-02 21:44:42 -0400

    testing for public url

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -1,51 +1,55 @@
-import React, { Component } from "react";
-import Header from "./components/Header";
-import Logo from "./components/Logo";
-import Search from "./components/Search";
-import Icons from "./components/Icons";
-import Apps from "./components/Apps";
+import React, { Component } from 'react'
+import Header from './components/Header'
+import Logo from './components/Logo'
+import Search from './components/Search'
+import Icons from './components/Icons'
+import Apps from './components/Apps'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      appsOpen: false
-    };
+	constructor(props) {
+		super(props)
+		this.state = {
+			appsOpen: false,
+		}
 
-    this.closeApps = this.closeApps.bind(this);
-    this.openApps = this.openApps.bind(this);
-  }
+		this.closeApps = this.closeApps.bind(this)
+		this.openApps = this.openApps.bind(this)
+	}
 
-  closeApps(e) {
-    if (this.state.appsOpen === true) {
-      this.setState(state => ({
-        appsOpen: !state.appsOpen
-      }));
-    }
-  }
+	componentDidMount() {
+		console.log(`public path: ${process.env.PUBLIC_URL}`)
+	}
 
-  openApps(e) {
-    if (this.state.appsOpen === false) {
-      this.setState(state => ({
-        appsOpen: !state.appsOpen
-      }));
-    }
-  }
+	closeApps(e) {
+		if (this.state.appsOpen === true) {
+			this.setState(state => ({
+				appsOpen: !state.appsOpen,
+			}))
+		}
+	}
 
-  render() {
-    return (
-      <div onClick={this.closeApps} className="body-container">
-        <Header openApps={this.openApps} />
-        <Apps closeApps={this.closeApps} appsOpen={this.state.appsOpen} />
+	openApps(e) {
+		if (this.state.appsOpen === false) {
+			this.setState(state => ({
+				appsOpen: !state.appsOpen,
+			}))
+		}
+	}
 
-        <div className="main-container">
-          <Logo />
-          <Search />
-          <Icons />
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div onClick={this.closeApps} className="body-container">
+				<Header openApps={this.openApps} />
+				<Apps closeApps={this.closeApps} appsOpen={this.state.appsOpen} />
+
+				<div className="main-container">
+					<Logo />
+					<Search />
+					<Icons />
+				</div>
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
