6db6c4c24fcf1c59bc8b51cc91a61b1f3425ba99
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-05-05 01:22:38 -0400

    fixing ref in app drawer

--- src/scripts/components/AppDrawer.js
+++ src/scripts/components/AppDrawer.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React from 'react'
 
 function AppDrawer() {
-  return (
-    <a href="#">
-      <svg className="app-drawer-icon">
-        <use xlinkHref="/svg/symbol-defs.svg#icon-grid" />
-      </svg>
-    </a>
-  );
+	return (
+		<a href="#">
+			<svg className="app-drawer-icon">
+				<use
+					xlinkHref={process.env.PUBLIC_URL + `/svg/symbol-defs.svg#icon-grid`}
+				/>
+			</svg>
+		</a>
+	)
 }
 
-export default AppDrawer;
+export default AppDrawer
